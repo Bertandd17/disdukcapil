1064f81240db8c6095a9d379a9eeea93ec0d1152
AbetManurung_038 <abetmanurung17@gmail.com>
2026-05-27 15:58:56 +0700

    fix: deduplicate migrate, add migrate to start.sh

--- railway/init-app.sh
+++ railway/init-app.sh
@@ -5,5 +5,7 @@ mkdir -p bootstrap/cache storage/framework/sessions storage/framework/views stor
 chmod -R a+rw bootstrap/cache storage
 
 php artisan migrate --force
-
-php artisan migrate --force
+php artisan storage:link || true
+php artisan config:cache
+php artisan event:cache
+php artisan view:cache
--- railway/start.sh
+++ railway/start.sh
@@ -4,6 +4,7 @@ set -e
 mkdir -p bootstrap/cache storage/framework/sessions storage/framework/views storage/framework/cache storage/framework/testing storage/logs
 chmod -R a+rw bootstrap/cache storage
 
+php artisan migrate --force
 php artisan optimize:clear
 php artisan storage:link || true
 php artisan config:cache
